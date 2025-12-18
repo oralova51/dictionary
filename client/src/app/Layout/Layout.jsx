d619802caeeb44dc941dcaf2036850f62b07e768
Holy-Farus <davvidkassuman@gmail.com>
2025-12-18 18:22:24 +0300

    footer

--- client/src/app/Layout/Layout.jsx
+++ client/src/app/Layout/Layout.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Navigation from "../../widgets/Navigation/Navigation";
 import { Outlet } from "react-router";
 import Container from "react-bootstrap/Container";
+import Footer from "../../widgets/Footer/Footer";
 
 export default function Layout() {
   return (
-    <>
+    <div className="layout-container">
       <Container>
         <Navigation />
         <Outlet />
       </Container>
-    </>
+      <Footer />
+    </div>
   );
 }
---
+++ client/src/widgets/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div>&copy; 2024 Dictionary App. All rights reserved.</div>
+    </footer>
+  );
+}
