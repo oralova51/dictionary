1e16aaeb5b09070b7f44b58019e78a8d28303044
mdirect <muza33@ya.ru>
2025-12-19 14:44:02 +0300

    otkat

--- server/src/app.js
+++ server/src/app.js
@@ -9,10 +9,6 @@ const app = express();
 serverConfig(app);
 
 app.use('/api', apiRouter);
-app.use(express.static(path.join(__dirname, '..', 'dist')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
-});
 
 const { PORT } = process.env || 3001;
 
